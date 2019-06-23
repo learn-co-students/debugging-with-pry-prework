d3d98cb6845f16a42949bb360eb21ccc8b93afff
Michael Evans <mmevansjr@gmail.com>
2019-06-22 17:06:39 -1000

    Done.

--- lib/pry_debugging.rb
+++ lib/pry_debugging.rb
@@ -1,4 +1,6 @@
 def plus_two(num)
-	num + 2
-	num
+	num = num + 2
+	return num
+=begin In this debugging problem, the plus_two method definition ended with the variable num on line 4 and therefore returned 3 instead of the expected value of 5.
+ I used binding.pry to debug the code. I ended up adding a return value to num and changed line 2 from num = 2 to num = num + 2 =end
 end
